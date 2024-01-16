8efe2d6df5516b4ea920e1dbffcde5abc0e6ad5b
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-01-15 18:25:15 -0700

    Handle turns between players in a game

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 /* server side javascript in node.js */
 
-//create a server on port 3000
-const io = require('socket.io')(3000, {
+const e = require('cors');
+
+const { Server } = require("socket.io");
+
+const io = new Server(3000, {
     cors: {
         origin: ["http://127.0.0.1:5501"],
     },
@@ -9,62 +12,101 @@ const io = require('socket.io')(3000, {
 
 let rooms = [];     //list of all room id's
 let roomInfo = {};  //info regarding all rooms
+
+let clientRoomMapping = {};
 let roomTurns = {};
 
-const Turn = {
-    PlayerOne: 1,
-    PlayerTwo: 2,
-    PlayerThree: 3
-}
-
-io.on('connection', socket => {
-    console.log("\n---connection established---", socket.id, "\n");
+io.on('connection', (socket) => {
+    console.log("\n---server connection established---", socket.id, "\n");
 
     //handling a new room being created
-    socket.on("create-game", roomCode => {
+    socket.on("create-game", (roomCode) => {
         debugLogs("create", socket.id, roomCode)
         rooms.push(roomCode);
         roomInfo[roomCode] = [];
         roomInfo[roomCode].push(socket.id);
-        roomTurns[roomCode] = socket.id;    //setting whose turn it is currently
+        clientRoomMapping[socket.id] = roomCode;
+        roomTurns[roomCode] = 1;
+        console.log(roomTurns.roomCode);
         socket.join(roomCode);
+        socket.emit("show-code", roomCode);
     });
 
     //handling an existing room being joined
     //@TODO: need to use the .to("room name") to only send moves to clients in the same room (do this at the end)
     socket.on("join-game", roomCode => {
-        console.log("~~server recieved the click~~");
+        console.log("--server recieved the join request--");
         if(rooms.includes(roomCode) && roomInfo[roomCode].length < 3) {
             debugLogs("join", socket.id, roomCode);
             roomInfo[roomCode].push(socket.id);
+            clientRoomMapping[socket.id] = roomCode;
             socket.join(roomCode);
             socket.emit("allow-join");
+            socket.emit("show-code", roomCode);
         }
-        console.log("All players inside this room: ", roomInfo[roomCode]);
+        if(roomInfo && roomInfo[roomCode].length == 3) {
+            let clientsInRoom = roomInfo[roomCode];
+            let currentTurn = roomTurns[roomCode];
+            socket.to(clientsInRoom[currentTurn-1]).emit("activate-event-listener");
+        }
+
+        console.log("All players inside this room: ", roomInfo.roomCode);
     });
 
-    socket.on("check-for-start", () => {
-        var allRooms = socket.rooms;
-        console.log(allRooms);
-    });
+    socket.on("switch-turns", () => {
+        let roomCode = clientRoomMapping[socket.id];
+        let clientsInRoom = roomInfo[roomCode];
+        let currentTurn = roomTurns[roomCode];
+        socket.emit("deactivate-event-listener");
+        roomTurns[roomCode] = (currentTurn % 3) + 1;
+        socket.to(clientsInRoom[currentTurn%3]).emit("activate-event-listener");
+    })
+
+    // socket.on("highlight-request", () => {
+    //     let roomNumber = clientRoomMapping[socket.id];
+    //     // console.log(`test: ${roomNumber}`);
+
+    //     let clientsInRoom = roomInfo[roomNumber];
+    //     // console.log(`clientInRoom test: ${clientsInRoom}`);
+        
+    //     let currTurn = roomTurns[roomNumber];
+    //     // console.log(`test currTurn: ${currTurn}, ${typeof currTurn}`);
+        
+    //     if(socket.id === clientsInRoom[currTurn-1]){
+    //         socket.emit("allow-highlight");
+    //     }
+    // });
 
     //handle highlighting
-    socket.on("highlight", (canvasX, canvasY) => {
+    socket.on("highlight-other-clients", (canvasX, canvasY) => {
         console.log(canvasX, canvasY);
-        socket.broadcast.emit("highlight-clients", canvasX, canvasY)
+        socket.to(clientRoomMapping[socket.id]).emit("allow-highlight-other-clients", canvasX, canvasY)
     });
 
+    // socket.on("move-request", () => {
+    //     //@TODO: put this in a function
+    //     let roomNumber = clientRoomMapping[socket.id];
+    //     let clientsInRoom = roomInfo[roomNumber];
+    //     let currTurn = roomTurns[roomNumber];
+
+    //     if(socket.id === clientsInRoom[currTurn-1]) {
+    //         socket.emit("allow-move");
+    //         currTurn = (currTurn % 3) + 1; //switching turns
+    //     }
+    // });
+
     //handling making a move
-    socket.on("move", () => {
-        socket.broadcast.emit("move-clients");
+    socket.on("move-other-clients", currTurn => {
+        socket.to(clientRoomMapping[socket.id]).emit("allow-move-other-clients");
     });
 });
 
 function debugLogs(mode, socketId, roomCode) {
     if(mode === "create") {
         console.log("ROOM CREATED, CREATOR ID:", socketId);
-        console.log("CODE:", roomCode);
+        console.log("ROOM CODE:", roomCode);
     } else {
         console.log("ROOM JOINED, JOINER ID:", socketId);
+        console.log("ROOM CODE:", roomCode);
     }
 }
